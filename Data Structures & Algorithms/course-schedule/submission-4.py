7167c3381ddca781c3ae7c5a0b6443c6d268a697
Michael Wang <122252833+michaelwsd@users.noreply.github.com>
2026-07-08 09:33:45 +1000

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i:[] for i in range(numCourses)}
+
+        for u, v in prerequisites:
+            graph[v].append(u)
+        
+        canTake = [False] * numCourses
+
+        def check(c, visited):
+            if c in visited:
+                return False
+            if not graph[c]:
+                return True 
+            
+            visited.add(c)
+
+            for pre in graph[c]:
+                if not check(pre, visited):
+                    return False 
+            
+            return True 
+        
+        for i in range(numCourses):
+            if not check(i, set()):
+                return False 
+        
+        return True
+
+            
